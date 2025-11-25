736db4ce4be9ca49a360eb6688b9e87b33d375b6
Schpser <schps.games@gmail.com>
2025-11-25 09:15:48 +0100

    Sum of a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for the sum of a list."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)
