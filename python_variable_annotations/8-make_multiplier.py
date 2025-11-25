29d0104a17f797c78c11ce9a7be7bc07f5e29fa7
Schpser <schps.games@gmail.com>
2025-11-25 09:36:20 +0100

    Return a function that multiplies floats

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -9,5 +9,5 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a function that multiplies a float by a multiplier."""
     def multiplier_function(x: float) -> float:
         return x * multiplier
-    
+
     return multiplier_function
