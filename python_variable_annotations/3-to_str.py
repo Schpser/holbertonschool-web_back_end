89823dfeb0a362703204ef4a759b0b42c9ac8a4f
Schpser <schps.games@gmail.com>
2025-11-25 08:58:50 +0100

    Convert float to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module containing the to_str function for converting float to string."""
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a floating-point number."""
+    return str(n)
