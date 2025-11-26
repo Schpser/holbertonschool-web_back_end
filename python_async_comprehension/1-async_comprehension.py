7bf5b8e5484a1f442910ec65c50a3a271bc5ef1d
Schpser <schps.games@gmail.com>
2025-11-26 10:38:04 +0100

    Collect 10 random numbers from previous created function

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for async comprehensions"""
+
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Coroutine that collects 10 random numbers using async comprehension"""
+
+    return [i async for i in async_generator()]
