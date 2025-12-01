6e2aecee35307856fd5994dc7bfeddc8c95eb2ac
Schpser <schps.games@gmail.com>
2025-12-01 15:58:35 +0100

    Pycodestyle

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
-""" 11-schools_by_topic.py """
+"""Module that provides stats about Nginx logs in MongoDB"""
 
 
-def schools_by_topic(mongo_collection, topic):
-    """List of schools matching the topic"""
+from pymongo import MongoClient
 
-    query = {"topics": topic}
-
-    schools = mongo_collection.find(query)
-
-    return list(schools)
+if __name__ == "__main__":
+    # Connect to MongoDB
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    
+    # Access logs database and nginx collection
+    db = client.logs
+    collection = db.nginx
+    
+    # Total number of documents
+    total_logs = collection.count_documents({})
+    print("{} logs".format(total_logs))
+    
+    # Methods count
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+    
+    # Status check count
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print("{} status check".format(status_check))
--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3
-""" 12-log_stats.py """
+"""Module that provides stats about Nginx logs in MongoDB"""
 
 
 from pymongo import MongoClient
 
-
-def log_stats():
-    """Provides statistics about Nginx logs stored in MongoDB"""
-
+if __name__ == "__main__":
+    # Connect to MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017')
-
+    
+    # Access logs database and nginx collection
     db = client.logs
     collection = db.nginx
-
+    
+    # Total number of documents
     total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
+    print("{} logs".format(total_logs))
+    
+    # Methods count
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print("\tmethod {}: {}".format(method, count))
     
+    # Status check count
     status_check = collection.count_documents({
         "method": "GET",
         "path": "/status"
     })
-    print(f"{status_check} status check")
-
-
-if __name__ == "__main__":
-    log_stats()
+    print("{} status check".format(status_check))
