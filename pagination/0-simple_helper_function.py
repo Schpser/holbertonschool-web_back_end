95777d725e8668bba6ead8aad9292fc529144778
Schpser <schps.games@gmail.com>
2025-12-01 01:15:25 +0100

    Add module doc

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
+"""Helper function for pagination"""
+
 
 def index_range(page: int, page_size: int) -> tuple:
     """Calculate index range for pagination"""
-    
-    if page < 1 or page_size < 1:
-        raise ValueError("page et page_size must be >= 1")
 
     start_index = (page - 1) * page_size
     end_index = page * page_size
