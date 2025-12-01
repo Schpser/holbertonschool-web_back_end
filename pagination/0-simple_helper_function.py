94993fabb5673b44fb05faf33c63e508c7395349
Schpser <schps.games@gmail.com>
2025-12-01 01:06:20 +0100

    Fix Return (tuple)

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,4 +6,4 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = page * page_size
 
-    return range(start_index, end_index)
+    return (start_index, end_index)
