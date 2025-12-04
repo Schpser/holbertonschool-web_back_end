6d4832429d42f03ff7121dd76bfff7e110fb5b8f
Schpser <schps.games@gmail.com>
2025-12-04 15:32:47 +0100

    Fix access

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 """Module that provides stats about Nginx logs in MongoDB"""
 
+
 from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-
     collection = client.logs.nginx
 
     total_logs = collection.count_documents({})
-    print("{} logs".format(total_logs))
 
-    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    counts = []
     for method in methods:
         count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        counts.append(count)
 
     status_check = collection.count_documents({
         "method": "GET",
         "path": "/status"
     })
+
+    print("{} logs".format(total_logs))
+    print("Methods:")
+    for i, method in enumerate(methods):
+        print("    method {}: {}".format(method, counts[i]))
     print("{} status check".format(status_check))
