003fa7bf3d2260ff2f58e0e5aaa4d37c6a4a2c64
Schpser <schps.games@gmail.com>
2025-12-10 11:38:24 +0100

    Use for...of operator (ES6)

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,16 +1,11 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
-
-  return fullBudget;
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+  
+  return newArray;
 }
