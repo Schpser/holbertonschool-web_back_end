8c787d2de6606f557dd8a280e58b2b7aa8b302e2
Schpser <schps.games@gmail.com>
2025-12-16 03:32:28 +0100

    Slice

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,8 @@
 export default function cleanSet(set, startString) {
-
-  if (startString === '') {
-    return '';
-  }
-
-  const arrayFromSet = Array.from(set);
-
-  const filtered = arrayFromSet
+  if (startString === '') return '';
+  
+  return Array.from(set)
     .filter(value => value.startsWith(startString))
     .map(value => value.slice(startString.length))
     .join('-');
-
-  return filtered;
 }
