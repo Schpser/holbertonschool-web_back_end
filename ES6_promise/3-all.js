4fed40a045a89a42fda7fa121fa488f9d3abd951
Schpser <schps.games@gmail.com>
2025-12-17 17:47:09 +0100

    Handle multiple promises (Parallel)

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,7 +7,7 @@ export default async function handleProfileSignup() {
       createUser()
     ]);
     
-    console.log(photo.body, user.firstName, user.lastName);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   } catch (error) {
     console.log('Signup system offline');
   }
