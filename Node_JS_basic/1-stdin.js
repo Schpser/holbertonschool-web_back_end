83c9adbcb259b0364bd739868bd590c25c9a3e30
Schpser <schps.games@gmail.com>
2026-01-05 13:20:34 +0100

    Using process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,23 +1,18 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.setEncoding('utf8');
+let name = '';
 
-if (!process.stdin.isTTY) {
-  let input = '';
-  
-  process.stdin.on('data', (chunk) => {
-    input += chunk;
-  });
-  
-  process.stdin.on('end', () => {
-    const name = input.trim();
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-  });
-} else {
-  process.stdin.once('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-    process.exit();
-  });
-}
+process.stdin.on('data', (data) => {
+  name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+
+});
+
+process.on('SIGINT', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
