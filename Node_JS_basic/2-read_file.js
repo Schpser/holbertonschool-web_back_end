8a815a4ba8966f71b5840a9725927ad550cdd8e2
Schpser <schps.games@gmail.com>
2026-01-05 13:57:48 +0100

    Reading a file synchronously

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -5,26 +5,28 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
     const students = lines.slice(1);
-    
+
     if (students.length === 0) {
       throw new Error('Cannot load the database');
     }
-    
+
     console.log(`Number of students: ${students.length}`);
-    
+
     const fields = {};
-    
-    students.forEach((student) => {
-      const [firstname, , , field] = student.split(',');
-      if (!fields[field]) fields[field] = [];
+
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
       fields[field].push(firstname);
     });
-    
-    // TRI ALPHABÉTIQUE des fields pour garantir CS avant SWE
+
     const sortedFields = Object.keys(fields).sort();
     
     sortedFields.forEach((field) => {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      const names = fields[field];
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     });
     
   } catch (error) {
