6aaab05e9077d2af5d721f1d9715a5d0f1eddf23
Schpser <schps.games@gmail.com>
2026-01-05 16:07:12 +0100

    Create a Server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello Holberton School!');
-  res.end();
-});
-
-app.listen(1245);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+}).listen(1245);
 
 module.exports = app;
