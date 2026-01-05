d8ba428df9bdd819fd9c2001d53abe677cc7ce20
Schpser <schps.games@gmail.com>
2026-01-05 15:54:18 +0100

    Create a Server

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = app;
