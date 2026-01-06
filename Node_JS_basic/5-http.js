eb64e8dd5e2928c54be494284abccf8862660671
Schpser <schps.games@gmail.com>
2026-01-06 10:04:01 +0100

    Node HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,57 +1,32 @@
 const http = require('http');
-const fs = require('fs').promises;
+const countStudents = require('./3-read_file_async');
 
 const databasePath = process.argv[2];
 
-if (!databasePath) {
-  console.error('Error: Missing database path');
-  console.error('Usage: node 5-http.js <database_path>');
-  process.exit(1);
-}
+const app = http.createServer(async (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-async function countStudents(path) {
-  try {
-    const data = await fs.promises.readFile(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
+  if (request.url === '/') {
+    response.end('Hello Holberton School!');
+  } else if (request.url === '/students') {
+    response.write('This is the list of our students\n');
 
-    if (students.length === 0) {
-      throw new Error('Cannot load the database');
-    }
+    const originalLog = console.log;
+    const logs = [];
+    console.log = (...args) => {
+      logs.push(args.join(' '));
+    };
 
-    let result = `Number of students: ${students.length}\n`;
-
-    const fields = {};
-    students.forEach((line) => {
-      const [firstname, , , field] = line.split(',');
-      if (!fields[field]) fields[field] = [];
-      fields[field].push(firstname);
-    });
-
-    const sortedFields = Object.keys(fields).sort();
-    sortedFields.forEach((field) => {
-      const names = fields[field];
-      result += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-    });
-
-    return result.trim();
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
-
-const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
     try {
-      const studentsList = await countStudents(databasePath);
-      res.end(`This is the list of our students\n${studentsList}`);
+      await countStudents(databasePath);
+      console.log = originalLog;
+      response.end(logs.join('\n'));
     } catch (error) {
-      res.end('This is the list of our students\nCannot load the database');
+      console.log = originalLog;
+      response.end('Cannot load the database');
     }
+  } else {
+    response.end();
   }
 });
 
